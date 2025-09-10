1f9f2d65362e2478e90efa3cecf6a0e3424dc84b
Ankur Karna <ankurkarna0100@gmail.com>
2025-09-10 16:14:34 +0530

    Create NaukriUploader.java

---
+++ src/main/java/com/ankur/NaukriUploader.java
@@ -0,0 +1,67 @@
+package com.ankur;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.io.FileInputStream;
+
+public class NaukriUploader {
+
+    // Utility to unzip profile
+    public static void unzip(String zipFilePath, String destDir) throws IOException {
+        File dir = new File(destDir);
+        if(!dir.exists()) dir.mkdirs();
+        byte[] buffer = new byte[1024];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = new File(destDir, zipEntry.getName());
+                if (zipEntry.isDirectory()) {
+                    newFile.mkdirs();
+                } else {
+                    new File(newFile.getParent()).mkdirs();
+                    Files.copy(zis, newFile.toPath());
+                }
+                zipEntry = zis.getNextEntry();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        WebDriverManager.edgedriver().setup();
+
+        String zipPath = "./selenium-profile/myprofile.zip";
+        String extractPath = "./selenium-profile/myprofile";
+
+        try {
+            // unzip profile
+            unzip(zipPath, extractPath);
+
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("user-data-dir=" + extractPath);
+
+            WebDriver driver = new EdgeDriver(options);
+
+            driver.get("https://www.naukri.com/mnjuser/profile");
+            Thread.sleep(5000);
+
+            WebElement uploadInput = driver.findElement(By.id("attachCV"));
+            uploadInput.sendKeys("./resume/AnkurKarna_Resume.pdf");
+
+            System.out.println("Resume uploaded successfully!");
+            driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
